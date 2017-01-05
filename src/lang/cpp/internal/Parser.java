3377636857fca162ac79c62f59c1452273265095
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2017-01-05 16:32:03 +0100

    new rascal-shell jar
    Former-commit-id: d09ef35b993a0c218d7405aa5ff5eac00dcdeca2

--- src/lang/cpp/internal/Parser.java
+++ src/lang/cpp/internal/Parser.java
@@ -215,6 +215,7 @@ import org.rascalmpl.value.IString;
 import org.rascalmpl.value.IValue;
 import org.rascalmpl.value.IValueFactory;
 
+@SuppressWarnings("restriction")
 public class Parser extends ASTVisitor {
 	private IValueFactory vf;
 	private AST builder;
