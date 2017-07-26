2ee550adfb7e58d1c30c915a3d873a6571fb7b05
Rodin Aarssen <rodin.aarssen@cwi.nl>
2017-07-26 10:28:06 +0200

    Removed some old stuff

--- src/lang/cpp/internal/Parser.java
+++ src/lang/cpp/internal/Parser.java
@@ -214,9 +214,6 @@ public class Parser extends ASTVisitor {
 	private BindingsResolver br = new BindingsResolver();
 	private TypeResolver tr;
 
-	boolean doTypeLogging = false;
-	ISourceLocation sourceLoc;
-
 	public Parser(IValueFactory vf) {
 		super(true);
 		this.shouldVisitAmbiguousNodes = true;
@@ -257,7 +254,6 @@ public class Parser extends ASTVisitor {
 	public IValue parseCpp(ISourceLocation file, IList includePath, IMap additionalMacros, IEvaluatorContext ctx) {
 		try {
 			setIEvaluatorContext(ctx);
-			sourceLoc = file;
 			FileContent fc = FileContent.create(file.getAuthority() + file.getPath(),
 					((IString) new Prelude(vf).readFile(file)).getValue().toCharArray());
 
@@ -395,7 +391,6 @@ public class Parser extends ASTVisitor {
 
 	public IValue parseExpression(IString expression, IEvaluatorContext ctx) throws CoreException, IOException {
 		setIEvaluatorContext(ctx);
-		this.sourceLoc = vf.sourceLocation(URIUtil.assumeCorrect("unknown://", "", ""));
 		if (!expression.getValue().endsWith(";"))
 			expression.concat(vf.string(";"));
 		String expr = "void main() {\n\t" + expression.getValue() + "\n}";
@@ -416,7 +411,7 @@ public class Parser extends ASTVisitor {
 		return stack.pop();
 	}
 
-	public synchronized IValue convertCdtToRascal(IASTTranslationUnit translationUnit) throws CoreException {
+	public IValue convertCdtToRascal(IASTTranslationUnit translationUnit) throws CoreException {
 		translationUnit.accept(this);
 		if (stack.size() == 1)
 			return stack.pop();
