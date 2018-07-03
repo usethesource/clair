e9d2eff6800ebfdc21576f810ca7625b47393c76
Rodin Aarssen <rodin.aarssen@cwi.nl>
2018-07-03 12:48:27 +0200

    Copyright update

--- src/lang/cpp/internal/TypeResolver.java
+++ src/lang/cpp/internal/TypeResolver.java
@@ -1,5 +1,5 @@
 /** 
- * Copyright (c) 2016-2017, Rodin Aarssen, Centrum Wiskunde & Informatica (CWI) 
+ * Copyright (c) 2016-2018, Rodin Aarssen, Centrum Wiskunde & Informatica (CWI) 
  * All rights reserved. 
  *  
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: 
