f5cee5613abba4896f51acba79b85219c5b1cb1b
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2023-04-28 11:00:40 -0400

    this fixes #52 by adding a test runner

---
+++ src/lang/cpp/internal/RunTests.java
@@ -0,0 +1,12 @@
+package lang.cpp.internal;
+
+import org.junit.runner.RunWith;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestPrefix;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestRunner;
+
+@RunWith(RascalJUnitTestRunner.class)
+@RascalJUnitTestPrefix("lang::cpp")
+public class RunTests {
+   // nothing goes here. The test runner picks up all the Rascal source files from the
+   // src folder automaticall and starts loading and running their tests.
+}
