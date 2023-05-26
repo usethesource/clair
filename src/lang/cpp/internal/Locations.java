4fed3d9a3dfbffff35ac01748ad112bfd42dcbf6
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2023-05-26 11:09:09 +0200

    improved stability of file name dealings by reusing URIUtil more in the core of clair

--- src/lang/cpp/internal/Locations.java
+++ src/lang/cpp/internal/Locations.java
@@ -3,6 +3,7 @@ package lang.cpp.internal;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringReader;
+import java.net.URISyntaxException;
 import java.util.UUID;
 
 import org.eclipse.cdt.core.dom.ast.IASTFileLocation;
@@ -26,46 +27,49 @@ public class Locations {
 
 	public ISourceLocation forNode(IASTNode node) {
 		try {
-		IASTFileLocation astFileLocation = node.getFileLocation();
+			IASTFileLocation astFileLocation = node.getFileLocation();
 
-		if (astFileLocation != null) {
-			String fileName = astFileLocation.getFileName();
+			if (astFileLocation != null) {
+				String fileName = astFileLocation.getFileName();
 
-			// TODO: JV; I find this looking kind of tricky. Where would the wrong
-			// slashes come from and should we not solve the problem at the source?
-			// is there a way to get a "File" abstraction or Resource from CDT?
-			fileName = fileName.replace('\\', '/');
-
-			if (fileName.trim().startsWith("|")) {
-				try {
-					ISourceLocation tmp = (ISourceLocation) locParser.read(vf, TypeFactory.getInstance().sourceLocationType(), new StringReader(fileName));
+				if (fileName.trim().startsWith("|")) {
+					// here we used Rascal loc syntax which ended up in the CDT's file path somehow
 					
-					return vf.sourceLocation(
-							tmp,
-							astFileLocation.getNodeOffset(), 
-							astFileLocation.getNodeLength());
-				} catch (FactParseError | FactTypeUseException | IOException e) {
-		
-					return unknownPreciseLoc();
+					try {
+						ISourceLocation tmp = (ISourceLocation) locParser.read(vf, TypeFactory.getInstance().sourceLocationType(), new StringReader(fileName));
+						
+						// that would be weird, and also we'd overwrite it below
+						assert !tmp.hasOffsetLength();
+
+						return vf.sourceLocation(
+								tmp,
+								astFileLocation.getNodeOffset(), 
+								astFileLocation.getNodeLength());
+					} 
+					catch (FactParseError | FactTypeUseException | IOException e) {
+						return unknownPreciseLoc();
+					}
 				}
-			}
+				else {
+					// this parses the fileName in an OS-ARCH dependent way
+					ISourceLocation loc = URIUtil.createFileLocation(fileName.trim());
 
-			if (!fileName.startsWith("/")) {
-				fileName = "/" + fileName;
-			}
-
-			return vf.sourceLocation(
-						vf.sourceLocation(fileName), 
+					return vf.sourceLocation(
+						loc, 
 						astFileLocation.getNodeOffset(),
 						astFileLocation.getNodeLength()
 					);
-		}
+				}
+			}
 
-		return unknownPreciseLoc();
-	}
-	catch (RuntimeException e) {
-		throw new RuntimeException("AST at " + node + " failed", e);
-	}
+			return unknownPreciseLoc();
+		}
+		catch (RuntimeException e) {
+			throw new RuntimeException("getting location for node " + node + " failed", e);
+		} 
+		catch (URISyntaxException e) {
+			throw new RuntimeException("location for not " + node + " has URISyntaxException: " + e, e);
+		}
 	}
 
 	private ISourceLocation unknownPreciseLoc() {
