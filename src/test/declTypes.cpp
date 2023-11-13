1b9fcfa6d18ed50e7ed126ea578c6d3ad8f3f9c5
Linus Wagner <linus.wagner@malighting.de>
2023-11-13 15:52:21 +0100

    add some more examples

--- src/test/declTypes.cpp
+++ src/test/declTypes.cpp
@@ -4,10 +4,13 @@
 
 class Person {
     static int F;
-    int hello() {
-        return F;
-    }
+    public:
+        static int G;
+        static int static_method() {
+            return F;
+        }
 };
+
 
 Person p;
 decltype(p) q = p;
@@ -19,6 +22,8 @@ P qq;
 
 decltype(qq) x;
 
+int some_f = decltype(p)::G;
 
-
-
+int some_number = 5;
+decltype(some_number) double_the_number = some_number * 2;
+int a = decltype(p)::static_method();
